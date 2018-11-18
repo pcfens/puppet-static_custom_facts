a173b01d88d689de5e7406e0ae8c9d6ee0fab1e9
rtib <rtib@users.noreply.github.com>
2018-11-18 13:21:51 +0100

    wide os coverage

--- spec/classes/static_custom_facts_spec.rb
+++ spec/classes/static_custom_facts_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe 'static_custom_facts', type: :class do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'defaults' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('static_custom_facts') }
+        it { is_expected.to contain_class('static_custom_facts::params') }
+        it do
+          is_expected.to contain_file('facts-directory').with(
+            ensure: 'directory',
+          )
+        end
+      end  
+    end
+  end
+
   context 'On a Linux system' do
     let :facts do
       {
